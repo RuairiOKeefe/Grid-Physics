54b110ec2c4f88519f1d9a9f017f7378405fcedb
AdamBlane <adam.blance@btinternet.com>
2017-01-30 16:23:51 +0000

    Merged Camera script with texture mapping
    We have combined mine and Adams code to have a basic prototype with
    texture mapping and camera with laser pointers

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class LaserPointer : MonoBehaviour
+{
+    private SteamVR_TrackedObject tracked;
+    public GameObject laserPrefab;
+    private GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitpoint;
+    private bool troo;
+    private Vector3 prevlocation;
+    private SteamVR_Controller.Device Control
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)tracked.index);
+        }
+    }
+    void Awake()
+    {
+        tracked = GetComponent<SteamVR_TrackedObject>();
+    }
+    // Use this for initialization
+    private void MoveLaser(RaycastHit hit)
+    {
+        laserTransform.position = Vector3.Lerp(tracked.transform.position, hitpoint, 0.5f);
+        laserTransform.LookAt(hitpoint);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
+    }
+    private void ShowLaser()
+    {
+        laser.SetActive(true);
+    }
+    void Start ()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(tracked.transform.position, transform.forward, out hit, 1000))
+        {
+            hitpoint = hit.point;
+            MoveLaser(hit);
+        }
+
+        if (Control.GetPress(SteamVR_Controller.ButtonMask.Trigger) && !troo && (prevlocation != hitpoint)) 
+        { 
+            if (Physics.Raycast(tracked.transform.position , transform.forward , out hit , 1000))
+            { 
+                troo = true;
+                ShowLaser();
+               // MoveLaser(hit);
+            }
+
+        }
+        else if (!Control.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            laser.SetActive(false);
+            troo = false;
+        }
+        prevlocation = hitpoint; 
+    }
+}
