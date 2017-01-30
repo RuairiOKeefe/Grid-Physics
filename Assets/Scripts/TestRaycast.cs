28c8d7c4705e68bbadeacad3bb887dc7f5d71cba
RuairiOKeefe <40165151@live.napier.ac.uk>
2017-01-30 17:01:04 +0000

    Added a script that will eventually create objects at the location raycast to on the grid, going home now...

---
+++ Assets/Scripts/TestRaycast.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRaycast : MonoBehaviour {
+
+    public GameObject gridObject;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            //if(Physics.Raycast(this.transform.position, this.transform.forward, out hit, 1000, layerMask = "grid",)
+        }
+		
+	}
+}
