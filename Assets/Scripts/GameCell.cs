fd83fba273c7c59d1322428ea8ece0d035e93b89
RuairiOKeefe <40165151@live.napier.ac.uk>
2017-02-02 16:44:46 +0000

    Added basic grid setup code

---
+++ Assets/Scripts/GameCell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCell
+{
+	//Removed monobehavior because I believe it is not required and reduces performance
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+	public void Set(int x, int y)
+	{
+		X = x;
+		Y = y;
+	}
+
+}
---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+	public int width;
+	public int height;
+
+	GameCell[,] cells;
+
+
+	// Use this for initialization
+	void Start () {
+		CreateGrid();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		print(cells[1,6].X.ToString()  + ", " + cells[1, 6].Y.ToString());
+	}
+
+    void CreateGrid()
+    {
+		cells = new GameCell[width, height]; //Create an multidimensional array of cells to fit the grid
+
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				GameCell cell = new GameCell();
+				cell.Set(x, y);
+				cells[x, y] = cell;
+			}
+		}
+
+	}
+}
