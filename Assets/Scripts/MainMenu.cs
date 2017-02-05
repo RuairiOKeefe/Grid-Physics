e42e5d8e1363bb5b94a8d31259717d43099d7d6c
Tom Cebula <MindlessShad@gmail.com>
2017-02-05 15:15:53 +0000

    Added functional lever to switch to game
    Working prototype of the main menu lever pull.
    
    -Make scene transition use SteamVR_LoadLevel to make it smoother.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public float timer = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.localPosition.z < -0.700 )
+        {
+            timer -= Time.deltaTime;
+
+            if(timer <= 0.0f)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+        else
+        {
+            timer = 3.0f;
+        }
+    }
+}
